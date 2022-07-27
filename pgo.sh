f3d52675f1ac19a926c65be78fbe7c2bb2eb9cbb
Dezhi Wu <wu543065657@163.com>
2022-07-27 12:47:48 +0800

    ci: add llvm deps

--- pgo.sh
+++ pgo.sh
@@ -4,6 +4,9 @@ rm -rf /tmp/pgo-data
 
 RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" cargo run --release --target $1 -p bench --features=simd -- -m 0.4 ./tests/data/chess.dat
 
+apt update
+apt install -y llvm
+
 llvm-profdata merge -o /tmp/pgo-data/merged.profdata /tmp/pgo-data
 
 maturin build --release --target $1 --features simd -o dist -i python3.10 -- -Cprofile-use=/tmp/pgo-data/merged.profdata
