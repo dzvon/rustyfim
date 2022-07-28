6ce42ccbe56996f33bc2f344f32c2292b31804f4
Dezhi Wu <wu543065657@163.com>
2022-07-28 11:58:48 +0800

    ci: fix command

--- pgo.sh
+++ pgo.sh
@@ -4,6 +4,6 @@ rm -rf /tmp/pgo-data
 
 RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" cargo run --release --target $1 -p bench --features=simd -- -m 0.4 ./tests/data/chess.dat
 
-cargo llvm-profdata merge -o /tmp/pgo-data/merged.profdata /tmp/pgo-data
+rust-profdata merge -o /tmp/pgo-data/merged.profdata /tmp/pgo-data
 
 maturin build --release --target $1 --features simd -o dist -i python3.10 -- -Cprofile-use=/tmp/pgo-data/merged.profdata
